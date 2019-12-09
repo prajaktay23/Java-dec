1d36de2ec0b62e0777203c3050ff9d7209033202
prajaktay23 <prajaktayadav525@gmail.com>
2019-12-09 16:12:57 +0530

    continue statement

---
+++ src/com/company/LogicalStatement/Continueloop.java
@@ -0,0 +1,12 @@
+package com.company.LogicalStatement;
+
+public class Continueloop {
+    public static void main(String[] args) {
+        for (int i=0;i<=10;i++){
+            if (i==5) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
